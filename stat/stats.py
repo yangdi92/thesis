c49eeefa76c54917aed88870c8bdf09a884e3c19
yangdi92 <yangdi9203@gmail.com>
2018-12-23 17:24:55 +0800

    add function used for collect bboxes

--- stat/stats.py
+++ stat/stats.py
@@ -2,9 +2,11 @@
 import os
 from preprocess.tools import get_files
 import xml.etree.cElementTree as ET
+from collections import defaultdict
 import sys
 import numpy as np
 import cv2
+import pickle
 def get_stats(path):
     xmls = get_files(path)
     labels = {}
@@ -52,7 +54,21 @@ def get_mean_of_rgb(path, njob=1):
             g += k[1]
             b += k[2]
     return r/nums_img, g/nums_img, b/nums_img
-
-    
+def get_stats_of_bboxes(xml_dir, save_dir):
+    xmls = get_files(xml_dir, ".xml")
+    statistics = defaultdict(list)
+    for xml in xmls:
+        tree = ET.ElementTree(file=xml)
+        objs = tree.findall("./object")
+        for obj in objs:
+            name = obj.find("./name")
+            xmin = obj.find('./bndbox/xmin').text
+            xmax = obj.find("./bndbox/xmax").text
+            ymin = obj.find("./bndbox/ymin").text
+            ymax = obj.find('./bndbox/ymax').text
+            statistics[name].append([int(k) for k in [xmin, xmax, ymin, ymax]])
+    with open(os.path.join(save_dir, 'statistics.pickle'), 'rb') as f:
+        pickle.dump(statistics, f)
+    #return statistics  
 if __name__ == "__main__":
         get_stats(sys.argv[1])
